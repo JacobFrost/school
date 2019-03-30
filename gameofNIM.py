f3d6d7e93697bc24cb5f95a3625556e3289708be
Lauren <32347257+SysBreak256@users.noreply.github.com>
2019-03-29 21:13:06 -0400

    Update gameofNIM.py

--- gameofNIM.py
+++ gameofNIM.py
@@ -1,70 +1,82 @@
-from random import randint
+"""
+This is a program that plays a game of NIM with the user. The goal of the game is to pick up the last stick.
+Unless you can do mod calculations in your head you have a very good chance at loosing. Are you up to the challenge?
+This ai is very good at winning so if you beat it you should definitely congratulate yourself.
+Lauren Chotenovsky
+March 29 2019
+"""
+from random import randint      # Imports random library
 
-class Pile:
-    def __init__(self, pile=22):
+
+class Pile:                         # Creates the class Pile which is used to track and remove sticks from the pile
+    def __init__(self, pile=22):       # Initializes self.pile to have a value of whatever is passed through or 22
         self.pile = pile
 
-    def pileStatus(self):
+    def pileStatus(self):           # Returns the current size of the stick pile
         return self.pile
 
-    def removepile(self, amount):
+    def removepile(self, amount):   # Removes the amount that the ai or player removes from the pile
         self.pile -= amount
 
 
-class Player:
-    def __init__(self, name="Bob", sticks=0):
+class Player:                       # Creates the class player which handles values for the ai and player
+    def __init__(self, name="Bob", sticks=0):   # Initializes each player with default values if none are provided
         self.sticks = sticks
         self.name = name
 
-    def getSticks(self):
+    def getSticks(self):        # getSticks function returns the number of sticks the referred to player has
         return self.sticks
 
-    def getName(self):
+    def getName(self):          # getName function returns the name the referred to player
         return self.name
     
-    def addSticks(self, amount):
+    def addSticks(self, amount):    # addSticks adds the amount of sticks taken from the pile to the player
         self.sticks += amount
     
 
-class Game:
-    def __init__(self):
+class Game:                     # Creates the class Game which handles all the main game tasks
+    def __init__(self):         # initializes the pile, player and ai to the appropriate classes
         self.pile = Pile()
         self.player = Player(input("What is your name?"))
         self.ai = Player("Ai")
 
-    def play(self):
-        whostart = input("Who do you want to start? (0 for you, 1 for ai)")
-        if whostart == "0":
-            print("\nGreat! You are starting first!")
-            self.pickUpP()
-        elif whostart == "1":
-            print("\nGreat! The computer will be starting first!")
-            self.pickUpC()
-        else:
-            print("Sorry but that is not a valid choice.")
+    def play(self):                     # play function sets up who is playing first
+        whostart = input("Who do you want to start? (0 for you, 1 for ai)")    # asks the user who they want to go first
+        while True:
+            if whostart == "0":
+                print("\nGreat! You are starting first!")
+                self.pickUpP()
+                break
+            elif whostart == "1":
+                print("\nGreat! The computer will be starting first!")
+                self.pickUpC()
+                break
+            else:
+                print("Sorry but that is not a valid choice.")
 
-    def pickUpC(self):
-        amount = self.pile.pileStatus() % 5
-        if amount != 0:
+    def pickUpC(self):      # sets up the function pickUpC with is picking up sticks for the ai
+        amount = self.pile.pileStatus() % 5     # finds remainder of the pile divided by 5
+        if amount != 0:                         # if it isn't 0 then it removes the mod amount from the pile
             self.pile.removepile(amount)
             self.ai.addSticks(amount)
-        else:
+        else:                                   # if the remainder is anything else then the ai removes a random amount
             amount = randint(1, 4)
             self.pile.removepile(amount)
             self.ai.addSticks(amount)
-        print("The Ai took", amount, "from the pile\n")
-        if self.pile.pileStatus() == 0:
+        print("The Ai took", amount, "from the pile\n")        # notifies the player of how much was taken
+        if self.pile.pileStatus() == 0:                     # checks to see if the pile is empty, if so then the ai wins
             self.win("ai")
-        self.pickUpP()
+        else:
+            self.pickUpP()              # changes to the players turn
 
-    def pickUpP(self):
-        while True:
+    def pickUpP(self):      # sets up the function pickUpP with is picking up sticks for the player
+        while True:         # gets the player to input a value, if it isnt an int then it loops
             try:
                 print("You have", self.player.getSticks(), "sticks and the ai has", self.ai.getSticks(), "sticks.")
                 amount = int(input(self.player.getName() + ", How many sticks do you want to pick up from"
                                                            " the " + str(self.pile.pileStatus()) + " stick pile?"))
-                if amount <= 4:
-                    if self.pile.pileStatus() >= amount:
+                if amount <= 4:         # checks to see if the inputted value isn't too much or too little
+                    if self.pile.pileStatus() >= amount:    # I couldn't shorten this code with &'s because it broke
                         if amount >= 1:
                             break
                         else:
@@ -79,23 +91,23 @@ class Game:
             except ValueError:
                 print("That is not a valid integer number\n\n")
         print("You removed", amount, "sticks from the pile.\n")
-        self.pile.removepile(amount)
-        self.player.addSticks(amount)
-        if self.pile.pileStatus() == 0:
+        self.pile.removepile(amount)            # removes the sticks from the pile
+        self.player.addSticks(amount)           # adds the sticks to the player
+        if self.pile.pileStatus() == 0:         # checks to see if the pile is empty, if so then the player has won
             self.win("player")
-        self.pickUpC()
+        else:
+            self.pickUpC()          # moves to the computers turn if the pile isnt empty
 
-    def win(self, winner):
-        if winner == "player":
+    def win(self, winner):      # creates the function win which takes an argument of a winner
+        if winner == "player":  # if the winner is the player then it prints a message letting the user know
             print("Congrats " + self.player.getName() + ", you picked up the last stick and won!")
-        elif winner == "ai":
+        elif winner == "ai":    # If the winner is the ai then it prints a message letting the user know
             print("Sorry " + self.player.getName() + ", the ai won.")
-        exit()
 
 
-def start():
-    game = Game()
-    while True:
+def start():        # This is the start of the game, allows the user to browse a menu and learn how the game works
+    game = Game()   # sets up game to be linked to the Game class
+    while True:     # prints a menu until the user leaves to play the game
         print("""
     Welcome to the game of Nim!
     
@@ -103,11 +115,11 @@ def start():
     1 - Rules
     2 - Play
         """)
-        choice = input("")
-        if choice == "0":
+        choice = input("")      # Lets the user choose if they want to exit, learn more or start
+        if choice == "0":       # exits
             print("Thank you for playing the game.")
-            exit()
-        elif choice == "1":
+            break
+        elif choice == "1":     # Teaches the user how to play the game
             print("""
     The Rules
     
@@ -115,11 +127,10 @@ def start():
     There are 22 sticks, beginning with either you or the computer, 1-4 sticks are chosen before the sides switch.
     This continues until there are no more sticks left. Whoever chose the last stick wins.
             """)
-        elif choice == "2":
+        elif choice == "2":     # Starts the game
             print("Welcome to the game!")
-            while True:
-                game.play()
-                break
+            game.play()     # Calls the class game to use the function play which starts the game.
+            break
 
 
 start()
